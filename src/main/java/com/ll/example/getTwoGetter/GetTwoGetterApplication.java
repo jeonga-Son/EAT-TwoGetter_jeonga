c6273755ee8c71ab6f370a7c36337861d76eace0
sh970901 <aaddss639@naver.com>
2022-08-11 09:49:16 +0900

    setting

---
+++ src/main/java/com/ll/example/getTwoGetter/GetTwoGetterApplication.java
@@ -0,0 +1,13 @@
+package com.ll.example.getTwoGetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetTwoGetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetTwoGetterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/example/getTwoGetter/GetTwoGetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.example.getTwoGetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetTwoGetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
