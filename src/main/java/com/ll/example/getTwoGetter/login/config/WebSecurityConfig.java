2780f81f4afd5ca017b8555dc0ea42947b05864d
sh970901 <aaddss639@naver.com>
2022-08-17 00:14:40 +0900

    모달오류수정

--- src/main/java/com/ll/example/getTwoGetter/login/config/WebSecurityConfig.java
+++ src/main/java/com/ll/example/getTwoGetter/login/config/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/account/login")
-//                .defaultSuccessUrl("/account/login")
+                .defaultSuccessUrl("/")
                 .permitAll()
                 .and()
                 .logout()
