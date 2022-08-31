4b488a8028e2158ab72213e5565bd81d19f3c0a8
dhk767 <dhk767@gmail.com>
2022-09-01 01:19:54 +0900

    1차 파일 분리 html

---
+++ src/main/resources/static/js/modal-board.js
@@ -0,0 +1,40 @@
+const boardModal = document.querySelector('.boardModal')
+const btnOpenPopup2 = document.querySelector('.build_marker');
+const btnClosePopup2 = document.querySelector('.boardModal_close');
+const btnClosePopup3 = document.querySelector('.boardModal_close2');
+
+var infoDiv = document.getElementById('centerAddr')
+var infoLng = document.getElementById('centerLatLng')
+
+btnOpenPopup2.addEventListener('click', () => {
+    boardModal.style.display = 'block';
+    document.getElementById('locate').value = infoDiv.innerText
+    //slice(시작인덱스, 끝인덱스)
+    //끝 인덱스 값의 요소는 제외하고 그 전까지 가져온다.
+    document.getElementById('lat').value = infoLng.innerText.split(',')[0].slice(1,-1)
+    document.getElementById('lng').value = infoLng.innerText.split(',')[1].slice(1,-1)
+});
+
+btnClosePopup2.addEventListener('click', () => {
+    boardModal.style.display = 'none';
+});
+
+btnClosePopup3.addEventListener('click', () => {
+    boardModal.style.display = 'none';
+});
+
+
+function deleteAccount(username){
+    if (!confirm("정말로 삭제하시겠습니까?")) {
+        alert("삭제가 취소되었습니다.")
+    } else {
+        $.ajax({
+            url:'/api/users/'+username,
+            type:'DELETE',
+            success: function (result){
+                alert('정상적으로 삭제 되었습니다.')
+                window.location.href="/account/login";
+            }
+        })
+    }
+}
---
+++ src/main/resources/static/js/modal-category.js
@@ -0,0 +1,109 @@
+/*<![CDATA[*/
+let boardInfo = /*[[${board}]]*/
+let user1 = /*[[${user}]]*/;
+/*]]>*/
+
+var arrLat = [];
+var arrLng = [];
+var positions = [];
+var arridBoard= [];
+
+var imageSrc2 = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+// 마커 이미지의 이미지 크기 입니다
+var imageSize2 = new kakao.maps.Size(35, 50);
+
+// 마커 이미지를 생성합니다
+var markerImage2 = new kakao.maps.MarkerImage(imageSrc2, imageSize2);
+
+const boardDetailModal = document.querySelector('.boardDetailModal');
+const boardDetailModal_close = document.querySelector('.boardDetailModal_close');
+const boardDetailModal_close2 = document.querySelector('.boardDetailModal_close2');
+
+showBoardMarker();
+
+function showBoardMarker() {
+
+    for (var i = 0; i < boardInfo.length; i++) {
+        arrLat[i] = boardInfo[i].lat;
+        arrLng[i] = boardInfo[i].lng;
+        arridBoard[i] = boardInfo[i].id;
+    }
+
+    for (var i = 0; i < boardInfo.length; i++) {
+        positions[i] = new kakao.maps.LatLng(arrLat[i], arrLng[i]);
+    }
+
+    for (var i = 0; i < positions.length; i++) {
+        addMarker(positions[i], arridBoard[i]);
+    }
+}
+
+function addMarker(positions2, idBoard2) {
+    // 마커를 생성합니다
+    var marker2 = new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        position: positions2, // 마커를 표시할 위치
+        image : markerImage2, // 마커 이미지
+        title: idBoard2
+    });
+
+    kakao.maps.event.addListener(marker2, 'click', function() {
+        var showBoardNickname= document.getElementById('showBoardNickname')
+        var showBoardLocate = document.getElementById('showBoardLocate')
+        var showBoardTitle = document.getElementById('showBoardTitle')
+        var showBoardType = document.getElementById('showBoardType')
+        var showBoardName = document.getElementById('showBoardName')
+        var showBoardOrder = document.getElementById('showBoardOrder')
+        var showBoardMin = document.getElementById('showBoardMin')
+        var showBoardDel = document.getElementById('showBoardDel')
+        var showBoardContent = document.getElementById('showBoardContent')
+        var makeBoardTime = document.getElementById('makeBoardTime')
+        var showBoardLat = document.getElementById('showBoardLat')
+        var showBoardLng = document.getElementById('showBoardLng')
+
+        if(user1==null){
+            alert("로그인 이후 가능합니다")
+            location.href="/account/login"
+        }
+        fetch(`/getMarkerBoard/${marker2.getTitle()}`)
+            .then(data=>data.json())
+            .then(responseData=>{
+                console.log(responseData)
+                let geocoder = new kakao.maps.services.Geocoder();
+
+                let callback = function(result, status) {
+                    if (status === kakao.maps.services.Status.OK) {
+                        showBoardLocate.innerText =result[0].address.address_name
+                    }
+                };
+                let coord = new kakao.maps.LatLng(responseData.lat, responseData.lng);
+                var boardlocate = geocoder.coord2Address(coord.getLng(), coord.getLat(), callback)
+                var createDate = responseData.createdDate
+                var createDate_ = createDate.substring(0, 4)+"년 "+createDate.substring(5, 7)+"월" +createDate.substring(8, 10)+"일 ";
+                var createDate__ = " "+createDate.substring(11,13)+"시"+createDate.substring(14,16)+"분"
+                makeBoardTime.innerText = createDate_
+                makeBoardTime.innerText+=createDate__
+                showBoardNickname.innerText = responseData.username
+                showBoardTitle.innerText = responseData.title
+                showBoardType.innerText = responseData.storeType
+                showBoardName.innerText = responseData.storeName
+                showBoardOrder.innerText = responseData.orderDetail
+                showBoardMin.innerText = responseData.minimumOrderAmount
+                showBoardDel.innerText = responseData.deliveryCharge
+                showBoardContent.innerText = responseData.content
+                showBoardLat.innerText = responseData.lat
+                showBoardLng.innerText = responseData.lng
+                }
+            )
+
+        boardDetailModal.style.display = 'block';
+    });
+}
+
+boardDetailModal_close.addEventListener('click', () => {
+    boardDetailModal.style.display = 'none';
+});
+
+boardDetailModal_close2.addEventListener('click', () => {
+    boardDetailModal.style.display = 'none';
+});
