612961a11c3cf2197fa22797f6473f34dfd966b6
sh970901 <aaddss639@naver.com>
2022-09-04 18:35:40 +0900

    js파일 생성

---
+++ src/main/resources/static/js/modal-chat.js
@@ -0,0 +1,50 @@
+const showProfileModal  = document.querySelector(".modal")
+function removeStorage(){
+    localStorage.removeItem("Lat")
+    localStorage.removeItem("Lng")
+}
+function showProfile(){
+    showProfileModal.style.display='block';
+}
+function closeProfile(){
+    showProfileModal.style.display='none';
+}
+const chatModal = document.querySelector('.chatRoomModal')
+function showChat(){
+    var showBoardNickname = document.getElementById('showBoardNickname')
+    if(user1.nickname==showBoardNickname.innerText){
+        alert("채팅은 다른 사용자와 가능합니다")
+    }else{
+        chatModal.style.display='block';
+    }
+}
+function closeChat(){
+    chatModal.style.display='none';
+}
+function postChat(){
+    var chatRoomTitle=  document.getElementById('chatRoomTitle')
+    var showBoardNickname = document.getElementById('showBoardNickname')
+
+    fetch("/chatApi/chatPost",{
+        method: 'POST',
+        headers:{
+            'content-type':'application/json'
+        },
+        body : JSON.stringify({
+            chatTitle: chatRoomTitle.value,
+            username: user1.nickname,
+            partner: showBoardNickname.innerText
+        }),
+    }).then((response)=>{
+        if(response.status==200){
+            location.href="chat/list";
+        }
+    })
+
+}
+
+function middlespace(){
+    var partnerLat= document.getElementById('showBoardLat').innerText
+    var partnerLng =document.getElementById('showBoardLng').innerText
+    location.href=`/middlePlaceMap/middleMap?lat=${partnerLat}&lng=${partnerLng}`;
+}
