f6fce5c4cd5583a278a9adceb790e22182e70a07
dhk767 <dhk767@gmail.com>
2022-09-05 02:38:51 +0900

    신고하기 기능 추가 중, UserService 메서드 명 수정

--- src/main/java/com/ll/example/getTwoGetter/login/controller/HomeController.java
+++ src/main/java/com/ll/example/getTwoGetter/login/controller/HomeController.java
@@ -1,19 +1,15 @@
 package com.ll.example.getTwoGetter.login.controller;
 
 import com.ll.example.getTwoGetter.Board.domain.entity.Board;
-import com.ll.example.getTwoGetter.Board.domain.repository.BoardRepository;
 import com.ll.example.getTwoGetter.Board.service.BoardService;
-import com.ll.example.getTwoGetter.login.Repository.UserRepository;
 import com.ll.example.getTwoGetter.login.Service.UserService;
 import com.ll.example.getTwoGetter.login.model.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -28,7 +24,7 @@ public class HomeController {
     public String home(@AuthenticationPrincipal UserDetails userDetails, Model model, HttpSession session){
         if(userDetails != null){
             String username = userDetails.getUsername();
-            User user = userService.findByUsename(username);
+            User user = userService.findByUserName(username);
             model.addAttribute("user",user);
         }
         List<Board> boards = boardService.findAll();
