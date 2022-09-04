3cc379112d026e6a9b2624c5f60c9c0e4ef0cfaa
dhk767 <dhk767@gmail.com>
2022-09-04 18:46:09 +0900

    로그인 시 오류(js파일로 리다이렉트 되는 오류), 로그아웃 시 맵 안뜨는 오류 수정 완료

--- src/main/java/com/ll/example/getTwoGetter/login/config/WebSecurityConfig.java
+++ src/main/java/com/ll/example/getTwoGetter/login/config/WebSecurityConfig.java
@@ -1,10 +1,12 @@
 package com.ll.example.getTwoGetter.login.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -37,6 +39,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .invalidateHttpSession(true)
                 .permitAll();
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth)
             throws Exception {
