bee855372bcae97739657c47b291347a801020fc
jeonga <jjeong0106@gmail.com>
2022-09-23 02:48:37 +0900

    우리동네사람들 거리순 정렬 수정

--- src/main/resources/static/js/modal-board.js
+++ src/main/resources/static/js/modal-board.js
@@ -35,20 +35,3 @@ editBoardClose2.addEventListener('click', () => {
 function boardSubmitClickEvent() {
     gpsMarkerd.setVisible(false);
 }
-
-let latitude = null; // 위도
-let longitude = null; // 경도
-
-// 위도, 경도 가져오기
-navigator.geolocation.getCurrentPosition(
-    (position) => {
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-    },
-);
-
-function showDistance(){
-    location.href=`/board/${latitude}/${longitude}`
-}
-
-
